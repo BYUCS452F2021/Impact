335f597b55bd02221bae7366f019019a5914daf5
Mason Spong <mason@Masons-MacBook-Air.local>
2021-09-17 10:47:15 -0600

    update Timer component with extra fields

--- impact-app/src/components/Timer.js
+++ impact-app/src/components/Timer.js
@@ -2,7 +2,19 @@
 function Timer({ name }) {
   return (
     <div className="timer">
-      {name}
+      <div className="timer-start">
+        Start
+      </div>
+      <div className="timer-name">
+        {name}
+      </div>
+      <div className="timer-time-elapsed">
+        1:00
+      </div>
+      <div className="timer-delete">
+        Delete
+      </div>
+      
     </div>
   );
 }
